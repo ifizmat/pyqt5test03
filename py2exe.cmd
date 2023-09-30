0f8692d9593d05e409adc0800fdd934135138db6
ifizmat <ifizmat@gmail.com>
2023-09-30 21:28:26 +0400

    Rename pyqt5test2.py to pyqt5test3.py

--- py2exe.cmd
+++ py2exe.cmd
@@ -1,1 +1,1 @@
-.\project_env\Scripts\pyinstaller --onefile --windowed pyqt5test2.py --noconsole --name test1 --noconfirm
+.\project_env\Scripts\pyinstaller --onefile --windowed pyqt5test3.py --noconsole --name test1 --noconfirm
